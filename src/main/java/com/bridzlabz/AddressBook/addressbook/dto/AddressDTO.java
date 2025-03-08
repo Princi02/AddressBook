53806f56003ec2657cd48d5292246cf133e373d2
Princi02 <priyanshimangal891@gmail.com>
2025-03-08 20:39:01 +0530

    add lombok for getter and setter

--- src/main/java/com/bridzlabz/AddressBook/addressbook/dto/AddressDTO.java
+++ src/main/java/com/bridzlabz/AddressBook/addressbook/dto/AddressDTO.java
@@ -1,7 +1,13 @@
 package com.bridzlabz.AddressBook.addressbook.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data  // Generates Getters, Setters, toString, equals, and hashCode
+@NoArgsConstructor  // Generates a no-args constructor
+@AllArgsConstructor // Generates a constructor with all fields
 public class AddressDTO {
 
     @NotBlank(message = "Name is required")
@@ -15,59 +21,4 @@ public class AddressDTO {
 
     @NotBlank(message = "Phone number is required")
     private String phoneNumber;
-
-    // Constructor
-    public AddressDTO() {
-    }
-
-    public AddressDTO(String name, String city, String state, String phoneNumber) {
-        this.name = name;
-        this.city = city;
-        this.state = state;
-        this.phoneNumber = phoneNumber;
-    }
-
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    // toString() Method
-    @Override
-    public String toString() {
-        return "AddressDTO{" +
-                "name='" + name + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/bridzlabz/AddressBook/addressbook/model/Address.java
+++ src/main/java/com/bridzlabz/AddressBook/addressbook/model/Address.java
@@ -2,8 +2,14 @@ package com.bridzlabz.AddressBook.addressbook.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data  // Lombok will generate Getters, Setters, toString, equals, and hashCode
+@NoArgsConstructor  // No-args constructor
+@AllArgsConstructor // Constructor with all fields
 public class Address {
 
     @Id
@@ -21,69 +27,4 @@ public class Address {
 
     @NotBlank(message = "Phone number is required")
     private String phoneNumber;
-
-    // Constructor
-    public Address() {
-    }
-
-    public Address(Long id, String name, String city, String state, String phoneNumber) {
-        this.id = id;
-        this.name = name;
-        this.city = city;
-        this.state = state;
-        this.phoneNumber = phoneNumber;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    // toString() Method
-    @Override
-    public String toString() {
-        return "Address{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                '}';
-    }
 }
